9f3bdaba39a3d0361688d95e1fa3bd3895ffd95c
Kenta Yoshikawa <kenta.yoshikawa.dev@gmail.com>
2025-11-18 02:18:01 +0900

    [UPDATE] sequalizeの内容を修正

--- server/src/sequelize.ts
+++ server/src/sequelize.ts
@@ -1,16 +1,15 @@
 import { Sequelize } from "sequelize";
-// import "dotenv/config";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASS!,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    dialect: "mysql",
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: "mysql" as const,
     logging: false,
   }
 );
