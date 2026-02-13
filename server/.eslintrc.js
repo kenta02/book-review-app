9c6e43c6324fec5e8d38d81c4d5f93b4279563f2
kenta02 <kenta.yoshikawa.dev@gmail.com>
2026-02-14 01:56:38 +0900

    [ADD] ESLint 設定ファイルと無視リストを追加

---
+++ server/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
+  rules: {
+    // Type safety
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+
+    // Recommended safety / style
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+
+    // Imports / unused
+    'unused-imports/no-unused-imports': 'error',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always'
+      }
+    ],
+
+    // Prevent dangerous comments / bypasses
+    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+
+    // Useful strictness but not blocking developer flow
+    '@typescript-eslint/explicit-module-boundary-types': 'warn'
+  },
+  settings: {
+    'import/resolver': {
+      node: { extensions: ['.js', '.ts'] },
+      typescript: {}
+    }
+  }
+};
